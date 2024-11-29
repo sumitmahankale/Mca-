d088fd288eda20c78847899609134570e306e7af
Sumit Mahankale <154894418+sumitmahankale@users.noreply.github.com>
2024-11-29 11:57:32 +0530

    Update Test4.java

--- Sem-1/JAVA/Test4.java
+++ Sem-1/JAVA/Test4.java
@@ -13,68 +13,6 @@ public class Test4 {
         
         return DriverManager.getConnection(url, username, password);
     }
-
-    
-    public static void createDatabaseAndTable() {
-        String createDBQuery = "CREATE DATABASE IF NOT EXISTS University";
-        String createTableQuery = """
-            CREATE TABLE IF NOT EXISTS Students (
-                student_id INT PRIMARY KEY,
-                first_name VARCHAR(50),
-                last_name VARCHAR(50),
-                age INT,
-                major VARCHAR(100)
-            )
-        """;
-        String insertSampleDataQuery = """
-            INSERT INTO Students (student_id, first_name, last_name, age, major)
-            VALUES 
-                (1, 'sumit', 'mahankale', 20, 'C1omputer Science'),
-                (2, 'vedant', 'shinde', 22, 'Mathematics'),
-                (3, 'pratik', 'choure', 21, 'Physics')
-        """;
-
-        try (Connection conn = getConnection();
-             Statement stmt = conn.createStatement()) {
-           
-            stmt.executeUpdate(createDBQuery);
-            stmt.executeUpdate("USE University");
-
-           
-            stmt.executeUpdate(createTableQuery);
-
-                        stmt.executeUpdate(insertSampleDataQuery);
-
-            System.out.println("Database and table created, sample data inserted.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    
-    public static void createStoredProcedure() {
-        String procedureQuery = """
-            DELIMITER $$
-
-            CREATE PROCEDURE getStudentById(IN studentId INT)
-            BEGIN
-                SELECT student_id, first_name, last_name, age, major
-                FROM Students
-                WHERE student_id = studentId;
-            END $$
-
-            DELIMITER ;
-        """;
-
-        try (Connection conn = getConnection();
-             Statement stmt = conn.createStatement()) {
-            stmt.executeUpdate(procedureQuery);
-            System.out.println("Stored procedure 'getStudentById' created.");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
     
     public static void getStudentById(int studentId) {
         String sql = "{CALL getStudentById(?)}";
